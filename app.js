44edc500367f8d60428e54f80edbac975c9c2526
VapidSlay <Camden179@gmail.com>
2017-05-21 12:37:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const client = new Discord.Client();
 const credentials = require('./credentials.json');
 const data = require('./data.json');
 
-client.on('disconnect', () => console.log('Selfbot has disconnected.'));
+client.on('disconnect', () => console.log('=====================\nDisconnected'));
 
-client.on('reconnect', () => console.log('Attempting to reconnect...'));
+client.on('reconnect', () => console.log('=====================\nReconnecting...'));
 
-client.on('ready', () => console.log('Selfbot has successfully connected.'));
+client.on('ready', () => console.log('Succesfully Connected\n=====================\nLogged Commands:\n====================='));
 
 client.on('message', msg => {
   if (!msg) return;
@@ -27,7 +27,29 @@ client.on('message', msg => {
   if (msg.guild) guildMember = msg.guild.member(msg.author);
 
   switch (command) {
-    case 'emoji':
+	case 'emote':
+	case 'emoji':
+      if (!args) {
+        msg.error('You must provide a message to emojify.');
+      } else {
+        const chars = Array.from(args.join(' ').toLowerCase());
+        let message = '';
+        for (let index in chars) {
+          if (!chars[index].match(/[a-z]/i)) {
+            if (chars[index] === ' ') {
+              message += '    ';
+            } else {
+              message += chars[index];
+            }
+          } else {
+            message += ':regional_indicator_' + chars[index] + ':';
+          }
+        }
+        msg.channel.send(message);
+      }
+      break;
+	case 'eemote':
+	case 'eemoji':
       if (!args) {
         msg.error('You must provide a message to emojify.');
       } else {
@@ -61,6 +83,13 @@ client.on('message', msg => {
         }
       }
       break;
+	case 'me':
+	  var currentDiscrim = client.user.discriminator, discrimString = client.user.discriminator.toString(), check = new RegExp("^(\d)(?!\1+$)\d{11}$");
+	  const meembed = new Discord.RichEmbed()
+	  .addField(client.user.username + '#' + discrimString, '<@' + client.user.id + '>' + '\nid: ' + client.user.id)
+	  .setColor(rand(data.embedColors));
+      msg.sendEmbed(meembed);
+	  break;
     case 'fe':
     case 'fakeeval':
       const fakecode = args.join(' ');
@@ -74,6 +103,12 @@ client.on('message', msg => {
         msg.sendEmbed(fakeCodeEmbed);
       }
       break;
+	case 'servers':
+	  const sembed = new Discord.RichEmbed()
+	  .addField('Servers', client.guilds.map(g=>g.name).join('\n'))
+	  .setColor(rand(data.embedColors));
+	  msg.sendEmbed(sembed);
+	  break;
     case 'e':
     case 'eval':
       const code = args.join(' ');
@@ -95,6 +130,14 @@ client.on('message', msg => {
         }
       }
       break;
+	case 'ut':
+	case 'uptime':
+	  var ut = parseFloat(((client.uptime)/(1000))).toFixed(0);
+	  var hours = ~~(ut/3600);
+	  var minutes = ~~((ut%3600)/60);
+	  var seconds = ut%60;
+	  msg.send('Uptime: '+hours+' hours, '+minutes+' minutes, '+seconds+' seconds');
+	  break;
     case 'stats':
     case 'statistics':
       const embed = new Discord.RichEmbed()
@@ -103,9 +146,23 @@ client.on('message', msg => {
         .addField('Memory', (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + ' MB', true)
         .addField('Users', client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString(), true)
         .addField('Servers', client.guilds.size.toLocaleString(), true)
-        .addField('Channels', client.channels.size.toLocaleString(), true)
+        .addField('Forked by', 'PapaJohn#7777', true)
         .setColor(rand(data.embedColors));
       msg.sendEmbed(embed);
+      break;
+	 case 'ss':
+	  var ssut = parseFloat(((client.uptime)/(1000))).toFixed(0);
+	  var hours = ~~(ssut/3600);
+	  var minutes = ~~((ssut%3600)/60);
+      const ssembed = new Discord.RichEmbed()
+        .addField('Author', 'John#0969', true)
+        .addField('Forked by', 'PapaJohn#7777', true)
+		.addField('Servers', client.guilds.size.toLocaleString(), true)
+		.addField('Memory', (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + ' MB', true)
+		.addField('Ping', client.ping.toFixed(2), true)
+		.addField('Uptime', hours+' hours, '+minutes+' minutes', true)
+        .setColor(rand(data.embedColors));
+	  msg.sendEmbed(ssembed);
       break;
     case '8':
     case '8ball':
@@ -120,6 +177,11 @@ client.on('message', msg => {
     case 'lenny':
       msg.channel.send('( ͡° ͜ʖ ͡°)');
       break;
+	case 'm':
+	case 'mem':
+	case 'memory':
+		msg.send('Memory: ' + (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2) + ' MB');
+		break;
     case 's':
     case 'shrug':
       msg.channel.send('¯\\_(ツ)_/¯');
@@ -142,6 +204,8 @@ client.on('message', msg => {
           });
         });
       break;
+    case 'r':
+	  break; 
     case 'reboot':
       msg.send('Rebooting...')
       .then(() => process.exit(1));
@@ -153,7 +217,9 @@ client.on('message', msg => {
           msg.send("This command does not exist.");
         }
       break;
+	  
   }
+  console.log('  .'+content);
 });
 
 Discord.Message.prototype.sendEmbed = function(spicyEmbed) {
@@ -167,46 +233,31 @@ Discord.Message.prototype.sendEmbed = function(spicyEmbed) {
   });
 };
 
-Discord.Message.prototype.send = function(description) {
+Discord.Message.prototype.send = function(content) {
   if (credentials.embedDefault) {
     const embed = new Discord.RichEmbed()
       .setColor(rand(data.embedColors))
-      .setDescription(description);
+      .setDescription(content);
     return this.sendEmbed(embed);
   } else {
-    return this.channel.send(description)
+    return this.channel.send(content)
     .catch((e) => this.error(e.message));
   }
 };
 
-Discord.Message.prototype.error = function(description) {
+Discord.Message.prototype.error = function(content) {
   if (credentials.embedDefault) {
     const embed = new Discord.RichEmbed()
       .setColor([255, 0, 0])
-      .setDescription(description);
-    return this.channel.send({ embed:embed })
-    .catch((e) => {
-      if (e.code === 403) {
-        this.channel.send("You do not have permission to send embeds here.");
-      } else {
-        this.channel.send(e.message);
-      }
-    });
+      .setDescription(content);
+    return this.sendEmbed(embed);
   } else {
-    return this.channel.send(description);
+    return this.channel.send(content);
   }
 };
 
 function rand(array) {
   return array[Math.floor(Math.random() * array.length)];
-}
-
-function handleError(channel, error) {
-  if (error.code === 403) {
-    channel.send("You do not have permission to do that.");
-  } else {
-    channel.send(error.message);
-  }
 }
 
 client.login(credentials.token);
