1f9f1d5efa9cedf24e0a0e732daa9dd4b53bebc7
Papa John <camden179@gmail.com>
2017-06-16 14:23:25 -0400

    making updating smoother

--- UpdateSelfbot.bat
+++ UpdateSelfbot.bat
@@ -1,6 +1,7 @@
 @echo off
 title Updating Selfbot...
-ECHO This could take a couple minutes, so just leave it alone, dont worry if you get any errors
+ECHO This could take a couple minutes, so just leave it alone, dont worry if you get any errors. Also DO NOT CLOSE this window. It will close itself.
+pause
 git pull
 npm i -S hydrabolt/discord.js
 title Finished Updating
