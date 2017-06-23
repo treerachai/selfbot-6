0a1c766cb59ad1a65b81c8eb01b01b58c41df507
Papa John <camden179@gmail.com>
2017-06-23 07:51:01 -0400

    Making updating process easier

--- UpdateSelfbot.bat
+++ UpdateSelfbot.bat
@@ -1,7 +1,11 @@
 @echo off
 title Updating Selfbot...
-ECHO This could take a couple minutes, so just leave it alone, dont worry if you get any errors. Also DO NOT CLOSE this window. It will close itself.
-pause
+ECHO This part should be quick and not give any errors
 git pull
+ECHO =
+ECHO Finished updating selfbot files, if you would like to update discord.js too, continue. If not, close this window
+pause
+ECHO Updating discord.js, you WILL get errors while doing this, don't worry.
+ECHO This window will close itself when it is finished, DO NOT CLOSE IT MANUALLY
 npm i -S hydrabolt/discord.js
 pause
