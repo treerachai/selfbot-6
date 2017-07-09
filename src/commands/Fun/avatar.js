f18283f40db3720b1c4533ccba4c6b817493afc4
Papa John <camden179@gmail.com>
2017-07-09 02:41:57 -0400

    adding avatar

---
+++ src/commands/Fun/avatar.js
@@ -0,0 +1,35 @@
+const patron = require('patron.js');
+const util = require('../../utility');
+const Discord = require('discord.js');
+
+class Avatar extends patron.Command {
+  constructor() {
+    super({
+      name: 'avatar',
+      aliases: ['a'],
+      group: 'fun',
+      description: 'Get the avatar of a user',
+      guildOnly: false,
+      args: [
+        new patron.Argument({
+          name: 'user',
+          key: 'user',
+          type: 'user',
+          example: 'PapaJohn#7777',
+          isRemainder: true,
+          default: patron.Default.Author
+        })
+      ]
+    });
+  }
+
+  async run(context, args) {
+     const embed = new Discord.RichEmbed()
+      .setTitle(args.user.tag + '\'s Avatar')
+      .setImage(args.user.displayAvatarURL);
+      
+      return util.Messenger.sendEmbed(context.channel, embed);
+  }
+}
+
+module.exports = new Avatar();
