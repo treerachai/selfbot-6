7a5187a768a25a3b4e24acf939246389741a3aff
Papa John <camden179@gmail.com>
2017-07-09 14:00:17 -0400

    adding game

---
+++ src/commands/Fun/game.js
@@ -0,0 +1,35 @@
+const patron = require('patron.js');
+const util = require('../../utility');
+const Discord = require('discord.js');
+
+class Game extends patron.Command {
+  constructor() {
+    super({
+      name: 'game',
+      aliases: ['g'],
+      group: 'fun',
+      description: 'Shows what game a user is playing',
+      guildOnly: false,
+      args: [
+        new patron.Argument({
+          name: 'user',
+          key: 'user',
+          type: 'user',
+          example: 'I don\'t know',
+          isRemainder: true,
+          default: patron.Default.Author
+        })
+      ]
+    });
+  }
+
+  async run(msg, args) {
+    if (args.user.presence.game !== null) {
+      return util.Messenger.send(msg.channel, '`' + args.user.tag + '` is playing: `' + args.user.presence.game.name + '`');
+    } else {
+      return util.Messenger.send(msg.channel, '`' + args.user.tag + '` is not currently playing a game');
+    }
+  }
+}
+
+module.exports = new Game();
