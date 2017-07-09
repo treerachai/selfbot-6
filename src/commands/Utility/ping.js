53bb814d03c3e6c6b6232c07bd207db955731164
Papa John <camden179@gmail.com>
2017-07-09 12:52:47 -0400

    adding ping

---
+++ src/commands/Utility/ping.js
@@ -0,0 +1,20 @@
+const patron = require('patron.js');
+const util = require('../../utility');
+const Discord = require('discord.js');
+
+class Ping extends patron.Command {
+  constructor() {
+    super({
+      name: 'ping',
+      group: 'utility',
+      description: 'See your ping',
+      guildOnly: false
+    });
+  }
+
+  async run(msg) { 
+      return util.Messenger.send(msg.channel, 'Ping: ' + msg.client.ping + ' ms');
+  }
+}
+
+module.exports = new Ping();
