2a5d06a6438c6d4914b859c3b871743906cc9b19
Papa John <camden179@gmail.com>
2017-07-09 00:41:52 -0400

    adding roll

---
+++ src/commands/Fun/roll.js
@@ -0,0 +1,30 @@
+const patron = require('patron.js');
+const util = require('../../utility');
+
+class Roll extends patron.Command {
+  constructor() {
+    super({
+      name: 'roll',
+      group: 'fun',
+      description: 'Rolls a random number',
+      guildOnly: false,
+      args: [
+        new patron.Argument({
+          name: 'Max Roll',
+          key: 'maxroll',
+          type: 'float',
+          example: '100',
+          isRemainder: true,
+          default: '6'
+        })
+      ]
+    });
+  }
+
+  async run(context, args) {
+      const roll = util.Random.nextInt(1, args.maxroll);
+      return util.Messenger.send(context.channel, ':game_die: You rolled a ' + roll + ' out of ' + args.maxroll);
+  }
+}
+
+module.exports = new Roll();
