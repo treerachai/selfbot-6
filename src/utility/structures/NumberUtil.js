15a52abbd4db6c9aa158ea48124b28276e153464
Papa John <camden179@gmail.com>
2017-07-10 10:08:43 -0400

    Removing days from mstotime

--- src/utility/structures/NumberUtil.js
+++ src/utility/structures/NumberUtil.js
@@ -4,8 +4,7 @@ class NumberUtil {
       milliseconds: parseInt((input % 1000) / 100),
       seconds: parseInt((input / 1000) % 60),
       minutes: parseInt((input / (1000 * 60)) % 60),
-      hours: parseInt((input / (1000 * 60 * 60)) % 24),
-      days: parseInt(input / (1000 * 60 * 60 * 24))
+      hours: parseInt(input / (1000 * 60 * 60)),
     };
   }
 }
