53d4df05f20e5ae0a06ee4dd6839f198b499f18b
Papa John <camden179@gmail.com>
2017-07-10 16:18:13 -0400

    Adding djs command

---
+++ src/commands/Utility/djs.js
@@ -0,0 +1,22 @@
+const patron = require('patron.js');
+const util = require('../../utility');
+const Discord = require('discord.js');
+
+class Djs extends patron.Command {
+  constructor() {
+    super({
+      name: 'djs',
+      aliases: ['discordjs', 'discord.js', 'd.js'],
+      group: 'utility',
+      description: 'Get a link to the documentation of the Discord.js version this Selfbot uses',
+      guildOnly: false
+    });
+  }
+
+  async run(msg) {
+    return util.Messenger.send(msg.channel, 'https://discord.js.org/#/docs/main/stable/general/welcome',
+     'This Selfbot uses Discord.js version: ' + Discord.version);
+  }
+}
+
+module.exports = new Djs();
