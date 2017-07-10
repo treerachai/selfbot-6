c2f6a8e3472453661582e48f8c47dd4c42694b41
Papa John <camden179@gmail.com>
2017-07-10 09:30:32 -0400

    Adding server command

---
+++ src/commands/Utility/server.js
@@ -0,0 +1,19 @@
+const patron = require('patron.js');
+const util = require('../../utility');
+
+class Server extends patron.Command {
+  constructor() {
+    super({
+      name: 'server',
+      group: 'utility',
+      description: 'Sends an invite to this Selfbot\'s server',
+      guildOnly: false
+    });
+  }
+
+  async run(msg) {
+    return util.Messenger.send(msg.channel, '[Click Here to Join this Selfbot\'s official server](https://discord.gg/zz9KTka)');
+  }
+}
+
+module.exports = new Server();
