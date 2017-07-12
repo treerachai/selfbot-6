22527fb26a9873e8971c848fa95a77b5a2065041
Papa John <camden179@gmail.com>
2017-07-12 10:40:43 -0400

    Adding guildstats

---
+++ src/commands/Fun/guildstats.js
@@ -0,0 +1,40 @@
+const patron = require('patron.js');
+const util = require('../../utility');
+const Discord = require('discord.js');
+
+class Guildstats extends patron.Command {
+  constructor() {
+    super({
+      name: 'guildstats',
+      aliases: ['gs', 'serverstats', 'ss'],
+      group: 'fun',
+      description: 'Get a guild\'s statistics',
+      guildOnly: true,
+      args: [
+        new patron.Argument({
+          name: 'guild',
+          key: 'guild',
+          type: 'guild',
+          example: 'Papa John\'s Selfbot',
+          isRemainder: true,
+          default: patron.Default.Guild
+        })
+      ]
+    });
+  }
+
+  async run(msg, args) {
+     const embed = new Discord.RichEmbed()
+       .setTitle(args.guild.name)
+       .setThumbnail(args.guild.iconURL);
+    if (args.guild.owner !== null) embed.addField('Guild Owner', '`' + args.guild.owner.user.tag + '`', true);
+      embed.addField('Members', args.guild.memberCount, true)
+       .addField('Channels', args.guild.channels.array().length, true)
+       .addField('Region', args.guild.region, true)
+       .addField('Created At', args.guild.createdAt.toString().substring(0, 16), true);
+
+    return util.Messenger.sendEmbed(msg.channel, embed);
+  }
+}
+
+module.exports = new Guildstats();
