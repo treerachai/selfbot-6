ba5ab94b33886d99ef0d05002dbbaf33bd87d9aa
Papa John <camden179@gmail.com>
2017-07-27 10:28:05 -0400

    Preparing for command logging

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
   client.on('ready', async () => {
     const d = new Date();
-    console.log(d.getHours() + ':' + d.getMinutes() + ' - Papa John\'s Selfbot has successfully connected.');
+    console.log(d.getHours() + ':' + d.getMinutes() + ' - Papa John\'s Selfbot has successfully connected.\n        \\/ Logged Commands \\/');
   });
 };
