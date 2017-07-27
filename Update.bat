308d088d9fc9e3e8b649597560329003d9e16004
Papa John <camden179@gmail.com>
2017-07-27 17:15:09 -0400

    No more local change errors

--- Update.bat
+++ Update.bat
@@ -1,7 +1,8 @@
 @echo off
 title Updating Papa John's Selfbot...
 ECHO Pulling the new files, this should be quick and errorless.
-git pull
+git fetch --all
+git reset --hard origin/master
 ECHO Updating the dependencies, any warning messages related to discord.js should be ignored.
 npm update
 pause
