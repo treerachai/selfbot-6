04dee364dca07abc674d37e54d06a4fe50160aa9
Papa John <camden179@gmail.com>
2017-08-02 20:09:18 -0400

    Making Altcheck look better

--- src/commands/Utility/altcheck.js
+++ src/commands/Utility/altcheck.js
@@ -24,13 +24,13 @@ class Altcheck extends patron.Command {
   }
 
   async run(msg, args) {
+    const gap = util.NumberUtil.msToTime(args.member.joinedTimestamp - args.member.user.createdTimestamp);
     const embed = new Discord.RichEmbed()
       .setAuthor(args.member.user.tag + ' (' + args.member.id + ')', args.member.user.displayAvatarURL)
-      .addField('Account Created', args.member.user.createdAt.toString().substring(0, 24))
-      .addField('Joined this Server', args.member.joinedAt.toString().substring(0, 24));
-    if (args.member.joinedTimestamp - args.member.user.createdTimestamp < 3600000) {
-      embed.setFooter('Joined server within 1 hour of account creation');
-    }
+      .addField('Account Created', args.member.user.createdAt.toString().substring(0, 24), true)
+      .addField('Create-Join Gap', gap.hours + ' hours, ' + gap.minutes + ' minutes', true)
+      .addField('Joined this Server', args.member.joinedAt.toString().substring(0, 24))
+      .setFooter(args.member.user.displayAvatarURL === args.member.user.defaultAvatarURL ? 'Uses Default Avatar' : 'Does Not Use Default Avatar');
     return util.Messenger.sendEmbed(msg.channel, embed);
   }
 }
